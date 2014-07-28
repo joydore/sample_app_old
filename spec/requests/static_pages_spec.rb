0e135d983347b46c3fb0c3620c5f9df09517017e
joydore <joy.dore@sage.com>
2014-07-28 15:39:24 +0100

    3.5 exercise working

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,9 +19,14 @@ describe "Static Pages" do
 
   describe "Contact page"  do
 
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 
@@ -34,7 +39,7 @@ describe "Static Pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -47,7 +52,7 @@ describe "Static Pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
 
   end
